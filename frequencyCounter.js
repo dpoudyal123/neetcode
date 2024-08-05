7f9d17dcdfbcb54a3a853250cbcb732f76cb7c27
dpoudyal123 <darshanpoudyal@yahoo.com>
2024-08-05 12:30:53 -0400

    first commit

---
+++ frequencyCounter.js
@@ -0,0 +1,5 @@
+/*
+    Frequency conter: 
+        This pattern uses objects or sets to collect values/frequency of values.
+        This can often avoid the need for nested loops O(n^2) operations with array/strings.
+*/
