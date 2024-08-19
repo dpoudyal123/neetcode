c5d7783a4228302371c712dd461a94de90bdbb82
dpoudyal123 <darshanpoudyal@yahoo.com>
2024-08-19 11:54:08 -0400

    top k retry

--- allNeetcode/Arrays&Hashing/topKFreqElements.js
+++ allNeetcode/Arrays&Hashing/topKFreqElements.js
@@ -10,20 +10,42 @@
 */
 
 function topKFrequent(nums, k) {
-  if (nums.length === 1) {
-    return nums;
+  const map = new Map();
+  for (let i of nums) {
+    const count = map.get(i) || 0;
+    map.set(i, count + 1);
   }
-  const obj = nums.reduce((agg, num) => {
-    agg[num] = agg[num] + 1 || 1;
-    return agg;
-  }, {});
-  return Object.keys(obj).reduce((agg, key) => {
-    const value = obj[key];
-    if (value > 1 && agg.length <= k) {
-      agg.push(key);
-    }
-    return agg;
-  }, []);
+  let i = 0;
+  result = [];
+  values = [...map.entries()].sort((a, b) => b[1] - a[1]);
+  while (i < k) {
+    result.push(values[i][0]);
+    i++;
+  }
+  return result;
+}
+// using this way time comp = O(nlogn) and space comp = O(n)
+
+/*
+  Another way is to solve using bucket sort time comp = O(n) and space = O(n)
+*/
+function topKFrequent2(nums, k) {
+  let map = new Map();
+  let bucket = [],
+    result = [];
+  for (let i of nums) {
+    let count = map.get(i) || 0;
+    map.set(i, count + 1);
+  }
+  for (let [ele, freq] of map) {
+    bucket[freq] = bucket[freq] ? bucket[freq].push(ele) : [ele];
+  }
+
+  for (let i = bucket.length - 1; i >= 0; i--) {
+    if (bucket[i]) result.push(...bucket[i]);
+    if (result.length >= k) break;
+  }
+  return result;
 }
 
-console.log(JSON.stringify(topKFrequent([1, 1, 1, 2, 2, 3], 2), null, 4));
+console.log(JSON.stringify(topKFrequent2([1, 1, 1, 2, 2, 3], 2), null, 4));
